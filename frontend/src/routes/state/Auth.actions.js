8215f2f37b6de7a80f51125d4816fb2a22534d86
Philip Ekblom <philip.ekblom@gmail.com>
2019-01-29 20:53:15 +0100

    Production build added

--- frontend/src/routes/state/Auth.actions.js
+++ frontend/src/routes/state/Auth.actions.js
@@ -4,15 +4,13 @@ export const AUTHENTICATED = "authenticated_user";
 export const UNAUTHENTICATED = "unauthenticated_user";
 export const AUTHENTICATION_ERROR = "authentication_error";
 
-const URL = "http://www.sample-website.com";
-
 export function signIn({ email, password }, history) {
   return async dispatch => {
     try {
-      // const res = await axios.post(`${URL}/signin`, { email, password });
+      const res = await axios.post(`api/login`, { email, password });
 
       dispatch({ type: AUTHENTICATED });
-      localStorage.setItem("user", email);
+      localStorage.setItem("user", res.token);
     } catch (error) {
       dispatch({
         type: AUTHENTICATION_ERROR,
