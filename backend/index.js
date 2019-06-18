e27c4a9d78468ebeb0cee928c34d2623a36a4085
Philip Ekblom <philip.ekblom@gmail.com>
2019-06-19 01:19:23 +0200

    Performance improvements

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const compression = require('compression');
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const api = require("./api");
 
+app.use(compression());
 app.use(cors({credentials: true, origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
