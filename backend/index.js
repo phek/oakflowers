69ec19787aa44a6d6d1c0b907a61efb1aff75c11
Philip Ekblom <philip.ekblom@gmail.com>
2019-06-20 01:47:59 +0200

    Changed port to 2053

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 2052;
+const port = process.env.PORT || 2053;
 const api = require("./api");
 
 app.use(compression());
