43a0d61ee0b47a1e15924c55ab19e2262f6d70f1
Philip Ekblom <philip.ekblom@gmail.com>
2019-06-20 02:13:08 +0200

    Changed port back to 5000

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 2053;
+const port = process.env.PORT || 5000;
 const api = require("./api");
 
 app.use(compression());
