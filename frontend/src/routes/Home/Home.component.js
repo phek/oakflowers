3d33858b309b9cc62090880f1893d31d963d789d
Philip Ekblom <philip.ekblom@gmail.com>
2019-06-21 18:33:24 +0200

    Updated home text

--- frontend/src/routes/Home/Home.component.js
+++ frontend/src/routes/Home/Home.component.js
@@ -17,10 +17,18 @@ const Home = () => {
             <Wrapper>
               <div className={styles.box}>
                 <Wrapper>
-                  <Heading level={1}>Hemsida under konstruktion</Heading>
+                  <Heading level={1} size='l'>Hemsida under konstruktion</Heading>
                   <Text color="text-secondary">
                     Sidan är under uppbyggnad och därmed kan du mötas av bland
-                    annat toma sidor och oklar funktionalitet.
+                    annat tomma sidor och oklar funktionalitet.
+                  </Text>
+                  <Text color="text-secondary">
+                    Det kan även hända att hemsidan ligger nere eller har olika
+                    problem under en kortare stund på grund av uppdatering av servern. Vid
+                    fel under en längre period, vänligen kontakta oss.
+                  </Text>
+                  <Text>
+                    Vi är även tacksamma för all feedback så hör gärna av er om förbättringar eller ifall något inte fungerar optimalt!
                   </Text>
                 </Wrapper>
               </div>
