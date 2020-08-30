168231e93850da5d1b1792e528779966511da31d
Philip Ekblom <philip.ekblom@gmail.com>
2020-08-30 18:02:08 +0200

    include build files

---
+++ frontend/build/precache-manifest.451880a71afa3abe3275bb23d8cd785c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a08301aa9e5257825824be0887e5e38d",
+    "url": "/static/media/logo.a08301aa.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f03bc0143665846953ec",
+    "url": "/static/js/main.59780388.chunk.js"
+  },
+  {
+    "revision": "c632333c58299a3e7316",
+    "url": "/static/js/2.923298b6.chunk.js"
+  },
+  {
+    "revision": "f03bc0143665846953ec",
+    "url": "/static/css/main.834d508c.chunk.css"
+  },
+  {
+    "revision": "6882155ea885c1f887a32ae8286ce926",
+    "url": "/index.html"
+  }
+];
