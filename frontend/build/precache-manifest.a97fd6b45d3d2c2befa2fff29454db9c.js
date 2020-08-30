83799b1882056a22708a38eb8c21d4cda885684c
Philip Ekblom <philip.ekblom@gmail.com>
2020-08-30 18:42:00 +0200

    Build FE

--- frontend/build/precache-manifest.a97fd6b45d3d2c2befa2fff29454db9c.js
+++ frontend/build/precache-manifest.a97fd6b45d3d2c2befa2fff29454db9c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6487c0cad8553c49da2a6b3248ad2ca1",
+    "revision": "ca124167250cf4463d34033f509cfdd3",
     "url": "/index.html"
   },
   {
-    "revision": "80b3e22ff240179cc95c",
+    "revision": "71f7cec92aa887c1bcb6",
     "url": "/static/css/main.5fbc48be.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e757a789.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "80b3e22ff240179cc95c",
-    "url": "/static/js/main.a205aa5f.chunk.js"
+    "revision": "71f7cec92aa887c1bcb6",
+    "url": "/static/js/main.8e17df39.chunk.js"
   },
   {
     "revision": "311c5b1bda3e1d56c157",
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c53af6b7c50453cc51b92e7e348d0e4.js"
+  "/precache-manifest.a97fd6b45d3d2c2befa2fff29454db9c.js"
 );
 
 self.addEventListener('message', (event) => {
