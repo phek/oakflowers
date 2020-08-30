642d2c82abe3abfda1f08a7653e0eb71789de437
Philip Ekblom <philip.ekblom@gmail.com>
2020-08-30 18:03:54 +0200

    Build FE

---
+++ frontend/build/precache-manifest.2c53af6b7c50453cc51b92e7e348d0e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6487c0cad8553c49da2a6b3248ad2ca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80b3e22ff240179cc95c",
+    "url": "/static/css/main.5fbc48be.chunk.css"
+  },
+  {
+    "revision": "2aa12b40edf259382d30",
+    "url": "/static/js/2.e757a789.chunk.js"
+  },
+  {
+    "revision": "6d264749db036879236f97fe37d201b0",
+    "url": "/static/js/2.e757a789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b3e22ff240179cc95c",
+    "url": "/static/js/main.a205aa5f.chunk.js"
+  },
+  {
+    "revision": "311c5b1bda3e1d56c157",
+    "url": "/static/js/runtime-main.88d1d10c.js"
+  },
+  {
+    "revision": "a08301aa9e5257825824be0887e5e38d",
+    "url": "/static/media/logo.a08301aa.png"
+  }
+]);
